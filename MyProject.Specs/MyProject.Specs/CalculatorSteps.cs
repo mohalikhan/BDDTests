7f579bc8af32f79759207f92a640f3e06ec83b28
Mohammad Ali <mohalikhan@hotmail.com>
2017-01-12 15:58:03 -0500

    First sample test to understand BDD from SpecFlow

---
+++ MyProject.Specs/MyProject.Specs/CalculatorSteps.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace MyProject.Specs
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private readonly Calculator _calculator = new Calculator();
+        private int Result { get; set; }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Result = _calculator.Add();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Result);
+        }
+    }
+}
